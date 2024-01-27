aa3522fff9d8d20a6ae7ef21d72a788b8e91d064
Mihail <vislapyz@mail.ru>
2024-01-27 14:38:07 +0300

    the function performed operations with the date

--- src/utils.py
+++ src/utils.py
@@ -5,3 +5,13 @@ def data_operations(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         data = json.load(file)
         return data
+
+
+def operations_from_the_list(data):
+    state_executed = []
+    for operation in data:
+        if 'state' in operation and  operation['state'] == 'EXECUTED':
+            state_executed.append(operation)
+
+    operation_sorted = sorted(state_executed, key=lambda x: x['date'], reverse=True)
+    return operation_sorted
