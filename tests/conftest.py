0fe1d74083642b287646a51e4a7df02517ef9c14
Mihail <vislapyz@mail.ru>
2024-01-28 22:05:00 +0300

    Adding a file conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,66 @@
+import pytest
+
+
+@pytest.fixture
+def test_fixture():
+    return [
+        {
+            "id": 587085106,
+            "state": "EXECUTED",
+            "date": "2018-03-23T10:45:06.972075",
+            "operationAmount": {
+                "amount": "48223.05",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Открытие вклада",
+            "to": "Счет 41421565395219882431"
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {
+                "amount": "67314.70",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657"
+        },
+        {
+            "id": 518707726,
+            "state": "EXECUTED",
+            "date": "2018-11-29T07:18:23.941293",
+            "operationAmount": {
+                "amount": "3348.98",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод с карты на карту",
+            "from": "MasterCard 3152479541115065",
+            "to": "Visa Gold 9447344650495960"
+        },
+        {
+            "id": 716496732,
+            "state": "EXECUTED",
+            "date": "2018-04-04T17:33:34.701093",
+            "operationAmount": {
+                "amount": "40701.91",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Gold 5999414228426353",
+            "to": "Счет 72731966109147704472"
+        },
+    ]
