e8f41985f0101ebc6f6e4051ad653a462a051131
Mihail <vislapyz@mail.ru>
2024-01-28 19:26:57 +0300

    The function receives the account and card data and converts them according to the task condition

--- src/utils.py
+++ src/utils.py
@@ -15,3 +15,19 @@ def operations_from_the_list(data):
 
     operation_sorted = sorted(state_executed, key=lambda x: x['date'], reverse=True)
     return operation_sorted
+
+
+def data_task_condition(data_number):
+    if 'Счет'in data_number:
+        n_account = data_number.slit(' ')[1]
+        name_account = data_number.split(' ')[0]
+        return  f'{n_account} ** {name_account[-4:]}'
+    elif 'Visa' in data_number:
+        card_number = data_number.split(' ')[-1]
+        card_name = data_number.split(' ')[0:-1]
+        full_the_card = ' '.json(card_name)
+        return f'{full_the_card} {card_number[0:4]} {card_number[5:7]}** **** {card_number[-4:]}'
+    else:
+        card_number = data_number.split(' ')[1]
+        card_name = data_number.split(' ')[0]
+        return f'{card_name} {card_number[0:4] } {card_number[5:7]}** **** {card_number[-4:]}'
